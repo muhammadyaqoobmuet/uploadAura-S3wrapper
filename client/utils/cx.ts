23933bd0172252b5a854ed4c8d0b809c69708c0a
Muhammad Yaqoob <muhammadyaqoobmuet@gmail.com>
2026-07-08 23:06:09 +0500

    cx comp

---
+++ client/utils/cx.ts
@@ -0,0 +1,10 @@
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cx(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+export function sortCx(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
