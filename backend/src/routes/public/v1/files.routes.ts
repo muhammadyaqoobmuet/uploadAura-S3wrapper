4cc02fe8b8ebc52003bcf2574e9e673a0ec24c96
Muhammad Yaqoob <muhammadyaqoobmuet@gmail.com>
2026-07-12 23:57:08 +0500

    fix bug validation

--- backend/src/routes/public/v1/files.routes.ts
+++ backend/src/routes/public/v1/files.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import { multiUpload } from '../../../config/multer-config';
 import { checkStorageMiddleware } from '../../../middlewares/check-storage';
+import { apiKeyAuthMiddleware } from '../../../middlewares/api-key-auth';
 import { uploadFileViaApi } from '../../../controllers/files.controller';
 
 
@@ -8,6 +9,7 @@ const fileV1Routes = Router();
 
 fileV1Routes.post(
   '/upload',
+  apiKeyAuthMiddleware,
   multiUpload,
   checkStorageMiddleware,
   uploadFileViaApi,
